3b87bd24e77db0a9b7ebff6fac1d79b6436d23a7
ahjee12 <ahjee200@gmail.com>
2020-07-30 12:17:04 +0900

    Updates

---
+++ precache-manifest.75ca1c7fdf025dad2ca89f97d51f4adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0481f860084c5d202859817d0ea73bb",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7c975964c3f6e7381380",
+    "url": "/movie_app_2020/static/css/main.2b7f077c.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c975964c3f6e7381380",
+    "url": "/movie_app_2020/static/js/main.17aaf591.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
